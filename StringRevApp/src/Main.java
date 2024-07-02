394c4a2004366c56a3bdc55956266476f7536e58
Vishal Yadav <vishal.yadav@bnt.local>
2024-07-02 12:41:28 +0530

    Implement try with resource

--- StringRevApp/src/Main.java
+++ StringRevApp/src/Main.java
@@ -6,23 +6,14 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        FileReader fileReader = null;
-        BufferedReader bufferedReader = null;
-        FileWriter fileWriter = null;
-        PrintWriter printWriter = null;
-        try {
-            fileReader = new FileReader("D://FirstTask-StringRevApplication//StringRevApp//string.txt");
-            bufferedReader = new BufferedReader(fileReader);
-
-            fileWriter = new FileWriter("D://FirstTask-StringRevApplication//StringRevApp//reversed_string.txt");
-            printWriter = new PrintWriter(fileWriter);
-
+        try(FileReader fileReader = new FileReader("D://FirstTask-StringRevApplication//StringRevApp//string.txt");
+            BufferedReader  bufferedReader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter("D://FirstTask-StringRevApplication//StringRevApp//reversed_string.txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter)){
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 String[] words = line.split(" "); // Split by whitespace (space, tab, etc.)
                 StringBuilder reversedLine = new StringBuilder();
-
                 for (String word : words) {
                     String reversedWord = reverseString(word);
                     reversedLine.append(reversedWord).append(" ");
@@ -33,28 +24,12 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        finally {
-            try {
-                if(fileWriter!=null){
-                    fileWriter.close();}
-                if(printWriter!=null){
-                    printWriter.close();}
-                if(fileReader!=null){
-                    fileReader.close();}
-                if(bufferedReader!=null){
-                    bufferedReader.close();}
-            }catch (Exception e)
-            {
-                System.out.println(e.getMessage());
-            }
-        }
     }
     private static String reverseString(String str) {
 
         char[] chars = str.toCharArray();
         int start = 0;
         int end = chars.length - 1;
-
         while (start < end) {
             if(chars[end]>=65&&chars[end]<=90||chars[end]>=97&&chars[end]<=122){
                 char temp = chars[start];
